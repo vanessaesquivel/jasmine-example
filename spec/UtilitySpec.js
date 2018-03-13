b9e0441e03b091e660bba80a83cd1d86cae5b359
Vanessa <vanessa.esquivel87@gmail.com>
2018-03-12 21:27:30 -0400

    add ageInYears utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -1,7 +1,13 @@
 describe("utility functions", function() {
   describe("fullName()", function() {
     it("concatenate two name parts with a space", function(){
-    expect(fullName("Billy","Bob")).toEqual("Billy Bob");  
+    expect(fullName("Billy","Bob")).toEqual("Billy Bob");
     });
   });
+  describe("ageInYears()",() => {
+    it("returns the age from a birthday", () => {
+      expect(ageInYears("1980-05-17")).toEqual(37);
+    });
+  });
+
 });
